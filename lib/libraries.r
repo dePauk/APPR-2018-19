61e4fdc947d652bab3efbe583d9c8bb3f4883805
Darjan Pavšič <darjan.pavsic@gmail.comgit config --global user.email darjan.pavsic@gmail.com>
2019-01-10 09:07:35 +0100

    Dodal nekaj knjižnic v mapo.

--- lib/libraries.r
+++ lib/libraries.r
@@ -3,6 +3,10 @@ library(rvest)
 library(gsubfn)
 library(reshape2)
 library(shiny)
+library(readr)
+library(dplyr)
+library(stringr)
+library(ggplot2)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
