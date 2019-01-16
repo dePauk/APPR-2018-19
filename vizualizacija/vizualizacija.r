faa8128160ccb4fe13618cc83f5132103be38747
Darjan Pavšič <darjan.pavsic@gmail.com>
2019-01-16 12:12:50 +0100

    Izbrisane nepotrebne vrstice

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,20 +1,5 @@
 # 3. faza: Vizualizacija podatkov
 
-# # Uvozimo zemljevid.
-# zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-#                              pot.zemljevida="OB", encoding="Windows-1250")
-# levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-#   { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-# zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-# zemljevid <- fortify(zemljevid)
-# 
-# # Izračunamo povprečno velikost družine
-# povprecja <- druzine %>% group_by(obcina) %>%
-#   summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
-
-
-## Uspešnost ekip v rednem delu VS Uvrstitve v Play-Offe
-#ggplot(data=podatki_ekipe_imensko, aes(x=podatki_ekipe_imensko$Uspesnost_redni_del, y=podatki_ekipe_imensko$Stevilo_playoffov)) + geom_point()
 
 # Uspešnost ekip v rednem delu VS Uspešnost ekip v Play-Offih
 ggplot(data=podatki_ekipe_imensko, aes(x=podatki_ekipe_imensko$Uspesnost_redni_del, y=podatki_ekipe_imensko$Playoff_uspesnost)) + geom_point()
@@ -39,7 +24,7 @@ ggplot(data=podatki_ekipe_imensko, aes(x=podatki_ekipe_imensko$Stevilo_playoffov
 -------------------------------
   
 # Uspešnost ekip v rednem delu VS Število osvojenih nazivov igralca tedna v tem obdobju  
-ggplot(data=podatki.join, aes(x=podatki.join$Uspesnost_redni_del, y=podatki.join$Stevilo)) + geom_point()  
+ggplot(data=podatki.join, aes(x=podatki.join$Uspesnost_redni_del, y=podatki.join$Stevilo)) + geom_point(size=2,color="dodgerblue3")  
   
   
 # Števiilo nazivov glede na pozicijo
