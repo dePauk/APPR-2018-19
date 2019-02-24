0b02dd72947fa3fe4ed4f1473015a062736b9c94
Darjan Pavšič <darjan.pavsic@gmail.com>
2019-02-24 14:37:30 +0100

    Posodobitev pri grafih

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -7,20 +7,12 @@ ggplot(data=podatki_ekipe_imensko, aes(x=podatki_ekipe_imensko$Uspesnost_redni_d
 # Uspešnost ekip v rednem delu VS Nastopi ekip v finalu
 ggplot(data=podatki_ekipe_imensko, aes(x=podatki_ekipe_imensko$Uspesnost_redni_del, y=podatki_ekipe_imensko$Nastopi_finale)) + geom_point()
 
-## Uspešnost ekip v rednem delu VS Število zmag lige NBA
-#ggplot(data=podatki_ekipe_imensko, aes(x=podatki_ekipe_imensko$Uspesnost_redni_del, y=podatki_ekipe_imensko$Zmage_finale)) + geom_point()
-
 # Število uvrstitev v Play-Offe VS Uspešnost ekip v Play-Offih
 ggplot(data=podatki_ekipe_imensko, aes(x=podatki_ekipe_imensko$Stevilo_playoffov, y=podatki_ekipe_imensko$Playoff_uspesnost)) + geom_point()
-
-##Število uvrstitev v Play-Offe VS Nastopi ekip v finalu
-#ggplot(data=podatki_ekipe_imensko, aes(x=podatki_ekipe_imensko$Stevilo_playoffov, y=podatki_ekipe_imensko$Nastopi_finale)) + geom_point()
 
 #Uspešnost ekip v rednem delu VS Število nazivov igralca tedna
 #zdruzi_ekipe_nazivi <- inner_join(podatki_ekipe_imensko, tabela_stevilo_nazivov_imena, by = "Ekipa")
 
-# TEST ZA RIBBON ----------------- #
-#ggplot(data=igralci_tedna, aes(x=igralci_tedna$Sezona_okrajsano, y=igralci_tedna$Starost)) + geom_ribbon(ymin=igralci_tedna$Starost -1, ymax=igralci_tedna$Starost +1)
 -------------------------------
   
 # Uspešnost ekip v rednem delu VS Število osvojenih nazivov igralca tedna v tem obdobju  
@@ -34,6 +26,20 @@ ggplot(data=podatki_ekipe_imensko, aes(x=podatki_ekipe_imensko$Stevilo_playoffov
 
 ggplot(data=statistika, aes(x=statistika$Starost, y=statistika$Odigrane_minute)) + geom_smooth(model=lm)
 
+
+
+
 # Bolj napredni grafi:
 
+### v grafih ena, dva izloči redke pozicije 
+
+## ena
 ggplot(data=igralci_tedna,aes(x=igralci_tedna$Pozicija, y=igralci_tedna$Visina)) + geom_jitter() + geom_boxplot(alpha=I(0.4))
+
+
+## dva
+ggplot(data=statistika,aes(x=statistika$Pozicija, y=statistika$Odigrane_minute)) + geom_jitter() + geom_boxplot(alpha=I(0.4))
+
+
+## test
+ggplot(data=statistika,aes(x=statistika$Pozicija, y=statistika$Odstotek_skoki_obramba)) + geom_jitter() + geom_boxplot(alpha=I(0.4))
