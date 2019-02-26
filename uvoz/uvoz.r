6614df2363f12db6128d612e69060f535eaaf05e
Darjan Pavšič <darjan.pavsic@gmail.com>
2019-02-26 18:47:16 +0100

    Urejanje kode

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -9,11 +9,12 @@ sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
 
 
-
 #-----------------------------------------------------------------------------------------------------------------------------------------
+
 # Funkcija, ki uvozi podatke o zmagah ekip v sezonah od 1984/85 do 2017/18.
 
 uvozi.rezultate <- function() {
+  
   link <- "http://mcubed.net/nba/nbaera.pl?year1=2000&year2=2018&sortby=rswin"
   stran <- html_session(link) %>% read_html()
   vrstice <- stran %>% html_nodes(xpath="//span[@class='hovl']") %>% html_text()
@@ -21,15 +22,12 @@ uvozi.rezultate <- function() {
   stolpci <- c("IZBRISI2","Sezon_v_ligi", "Ekipa", "Zmage_redni_del", "Porazi_redni_del", "Uspesnost_redni_del","Stevilo_playoffov", "Playoff_zmage", "Playoff_porazi","Playoff_uspesnost", "Zmage_serij","Porazi_serij","Uspesnost_serij","Nastopi_finale", "Zmage_finale","IZBRISI")
   
   podatki_ekipe <- read_csv(csv, locale=locale(encoding="UTF-8"), col_names=stolpci)
-  
   podatki_ekipe <- podatki_ekipe[,-c(1,2,16)]
 
   odstotki1 <- podatki_ekipe$Uspesnost_redni_del
   odstotki1 <- as.numeric(sub("%","",podatki_ekipe$Uspesnost_redni_del,fixed=TRUE))/100 
-  
   odstotki2 <- podatki_ekipe$Playoff_uspesnost
   odstotki2 <- as.numeric(sub("%","",podatki_ekipe$Playoff_uspesnost,fixed=TRUE))/100
-  
   odstotki3 <- podatki_ekipe$Uspesnost_serij
   odstotki3 <- as.numeric(sub("%","",podatki_ekipe$Uspesnost_serij,fixed=TRUE))/100
   
@@ -39,7 +37,6 @@ uvozi.rezultate <- function() {
   
   podatki_ekipe_imensko <- podatki_ekipe[order(podatki_ekipe$Ekipa),]
   
-
   ekipe_okrajsano = c("ATL","BOS","BKN","CHA","CHI","CLE","DAL","DEN","DET",
                       "GSW","HOU","IND","LAC","LAL","MEM","MIA","MIL","MIN",
                       "NOP","NYK","OKC","ORL","PHI","PHX","POR","SAC","SAS",
@@ -49,8 +46,6 @@ uvozi.rezultate <- function() {
   podatki_ekipe_imensko[,c(1,14,2,3,4,5,6,7,8,9,10,11,12,13)]
 
   #View(podatki_ekipe_imensko)
-  
-  
   return(podatki_ekipe_imensko)
 
 }
@@ -58,11 +53,8 @@ uvozi.rezultate <- function() {
 podatki_ekipe_imensko <- uvozi.rezultate()
 
 
-
-
-
-
 #--------------------------------------------------------------------------------------------------------------------------------------------------------
+
 #Funkcija, ki uvozi vse igralce tedna v sezonah od 1984/85 do 2017/18.
 
 uvozi.igralce_tedna <- function(){
@@ -70,15 +62,13 @@ uvozi.igralce_tedna <- function(){
   stolpci_igralci <- c("Starost","Konferenca","Datum_nagrade","Leto_drafta", "Visina", "Ime","Pozicija", "Sezona", "Sezona_okrajsano", "Stevilo_sezon_v_ligi", "Igralceva_ekipa", "Teza", "Vrednost_naziva")
   igralci_tedna <- read_csv("podatki/NBA_player_of_the_week.csv", locale = locale(encoding="UTF-8"), col_types = cols("Teza" = col_skip()), col_names = stolpci_igralci, skip=1)
 
-
   # Za prikaz sezone bo dovolj le en stolpec in sicer bom uporabil tistega s končno letnico.
   # Število sezon v ligi pomeni število končani sezon, torej "Rookie-ji" imajo pri tej spremenljivki vrednost 0.
   
   igralci_tedna <- igralci_tedna[,-c(3,8)]
 
-
-#  igralci_tedna$Teza <- (igralci_tedna$Teza) * 0.453592
-#  igralci_tedna$Teza <- signif(igralci_tedna$Teza, digits = 3)  
+  # igralci_tedna$Teza <- (igralci_tedna$Teza) * 0.453592
+  # igralci_tedna$Teza <- signif(igralci_tedna$Teza, digits = 3)  
   
   igralci_tedna$Visina <- igralci_tedna$Visina %>% strapplyc("([0-9]+)") %>% sapply(function(x) {
     x <- parse_number(x)
@@ -91,19 +81,15 @@ uvozi.igralce_tedna <- function(){
   
   igralci_tedna$Visina <- signif(igralci_tedna$Visina, digits = 3)
   
+  #View(igralci_tedna)
   return(igralci_tedna)
 }
-
-#igralci_tedna %>% View
 
 igralci_tedna <- uvozi.igralce_tedna()
 
 
-
-
-
-
 #--------------------------------------------------------------------------------------------------------------------------------
+
 # Funckija, ki iz tabele igralci_tedna prešteje število dobljenih nazivov igralca tedne vsake ekipe.
 
 
@@ -120,20 +106,16 @@ stevilo_nazivov_ekipa <- table(igralci_tedna$Igralceva_ekipa)
 
 tabela_stevilo_nazivov <- as.data.frame(table(igralci_tedna$Igralceva_ekipa))
 tabela_stevilo_nazivov <- tabela_stevilo_nazivov[order(tabela_stevilo_nazivov$Var1),]
-#tabela_stevilo_nazivov %>% View
 
 tabela_stevilo_nazivov2 <- tabela_stevilo_nazivov
-
 tabela_stevilo_nazivov2[3,2] <- 36
 tabela_stevilo_nazivov2[35,2] <- 31
 tabela_stevilo_nazivov2[24,2] <- 61
 tabela_stevilo_nazivov2[5,2] <- 26
 tabela_stevilo_nazivov2[22,2] <- 17
-
 tabela_stevilo_nazivov2 <- tabela_stevilo_nazivov2[-c(4,20,21,31,34),]
 
 rownames(tabela_stevilo_nazivov2) <- 1:nrow(tabela_stevilo_nazivov2)
-#tabela_stevilo_nazivov2 %>% View
 
 tabela_stevilo_nazivov_imena <- tabela_stevilo_nazivov2
 tabela_stevilo_nazivov_imena <- as.character(tabela_stevilo_nazivov2$Var1) %>%
@@ -145,85 +127,66 @@ tabela_stevilo_nazivov_imena <- as.character(tabela_stevilo_nazivov2$Var1) %>%
 stevilo_nazivov <- tabela_stevilo_nazivov2 %>%
   transmute(Ekipa = tabela_stevilo_nazivov_imena, Stevilo = Freq)
 
-#stevilo_nazivov %>% View
-
-
-
-
 
 #-------------------------------------------------------------------------------------------------------------------------------------------
-# Funkcija, ki uvozi statistiko velikega števila igralcev v ligi v sezonah od 1984/85 do 2017/18.
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
 
-
-uvozi.sezonsko_stat <- function(){
-
-  stolpci_statistika <- c("IZBRISI3", "Leto", "Igralec", "Pozicija", "Starost", "Ekipa", "Stevilo_iger", "Stevilo_zacetih_iger", "Odigrane_minute", "Player_efficiency_rating", "True_shooting", "3pt_attempt_rate", "Ft_rate", "Odstotek_skoki_napad", "Odstotek_skoki_obramba", "Odstotek_asistenc", "Ukradene_zoge", "Blokirani_meti", "Odstotek_izgubljenih_zog")
-  statistika <- read_csv("podatki/Seasons_Stats.csv", locale = locale(encoding="UTF-8"), col_names = stolpci_statistika, skip = 7216)
-  statistika <- statistika[,-c(1,12)]
-  
-  return(statistika)
-  #statistika %>% View
-}
-
-
-#MOGOČE SPLOH NE BOM UPORABLJAL TEGA
-#statistika <- uvozi.sezonsko_stat()
-
-
-
-
+# # Funkcija, ki uvozi statistiko velikega števila igralcev v ligi v sezonah od 1984/85 do 2017/18.
+# #
+# #
+# 
+# 
+# 
+# uvozi.sezonsko_stat <- function(){
+# 
+#   stolpci_statistika <- c("IZBRISI3", "Leto", "Igralec", "Pozicija", "Starost", "Ekipa", "Stevilo_iger", "Stevilo_zacetih_iger", "Odigrane_minute", "Player_efficiency_rating", "True_shooting", "3pt_attempt_rate", "Ft_rate", "Odstotek_skoki_napad", "Odstotek_skoki_obramba", "Odstotek_asistenc", "Ukradene_zoge", "Blokirani_meti", "Odstotek_izgubljenih_zog")
+#   statistika <- read_csv("podatki/Seasons_Stats.csv", locale = locale(encoding="UTF-8"), col_names = stolpci_statistika, skip = 7216)
+#   statistika <- statistika[,-c(1,12)]
+#   
+#   return(statistika)
+#   #statistika %>% View
+# }
+# 
+# 
+# #MOGOČE SPLOH NE BOM UPORABLJAL TEGA
+# #statistika <- uvozi.sezonsko_stat()
 
 
 #----------------------------------------------------------------------------------------------------------------------------------------
-# Funkcija, ki uvozi podatke o All-star igralcih.
 
+# Funkcija, ki uvozi podatke o All-star igralcih.
 
 uvozi.all_star <- function(){
   
   stolpci_allstar <- c("Igralec","Stevilo_allstar_nastopov","Sezone_allstar")
-  
   link <- "https://en.wikipedia.org/wiki/List_of_NBA_All-Stars"
   stran <- html_session(link) %>% read_html()
   tabela_allstar <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>% .[[1]] %>% html_table() %>%
     mutate(Player=Player %>% strapplyc("^([[:alnum:] '.-]*)") %>% unlist())
   
-  tabela_allstar <- tabela_allstar[,-5]
-  tabela_allstar <- tabela_allstar[,-4]
-  
+  tabela_allstar <- tabela_allstar[,-c(4,5)]
+
   names(tabela_allstar) <- stolpci_allstar
   
   return(tabela_allstar)
-  tabela_allstar %>% View
+  #tabela_allstar %>% View
 }
-
 
 tabela_allstar <- uvozi.all_star()
 
 
-
-
+##ČE IZBRIŠEŠ, IZBRIŠI TUDI V README.MD IN PROJEKT.RMD
 
 #--------------------------------------------------------------------------------------------------------------------------------------------
+
+# Dodatek:
+
 podatki.join <- inner_join(x = podatki_ekipe_imensko, y= stevilo_nazivov, by = "Ekipa") # %>% select("Ekipa", "Stevilo", "Uspesnost_redni_del")
-#View(podatki.join)
 
 igralci_tedna_pozicijefilt <- igralci_tedna[igralci_tedna$Pozicija != "G-F" & igralci_tedna$Pozicija != "F-C",]
 
-stevilo_nazivov_zvezne = c(0,0,45,0,44+27+71+23,41,0,0,31,57+17,32,0,0,44,23,0,0,0,17,0,0,43,29,26,0,0,0,0,0,0,0,0,36+36,26,0,59,61,33,37,0,0,0,9,30+56+61,47,0,0,0,0,26,0)
+stevilo_nazivov_zvezne = c(0,0,45,0,44+27+71+23,41,0,0,31,57+17,32,0,0,44,
+                           23,0,0,0,17,0,0,43,29,26,0,0,0,0,0,0,0,0,36+36,
+                           26,0,59,61,33,37,0,0,0,9,30+56+61,47,0,0,0,0,26,0)
 
 nazivi_zvezne <- statepop
 nazivi_zvezne$Nazivi <- stevilo_nazivov_zvezne
