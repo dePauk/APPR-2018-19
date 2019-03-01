e54e95682509a09227cd1bf0665d933663f631bc
Darjan Pavšič <darjan.pavsic@gmail.com>
2019-03-01 19:15:51 +0100

    Dodan graf za napredno analizo

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,15 @@
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
 
-# Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+#6 Napoved števila igralcev tedna za centre
+
+tabela_centri_vmesno <- igralci_tedna %>% filter(Pozicija == "C")
+stetje <- count(tabela_centri_vmesno, Sezona_okrajsano)
+tabela_centri <- as.data.frame(stetje)
+pril_lm <- lm(data=tabela_centri, n~Sezona_okrajsano)
+nova_leta <- data.frame(Sezona_okrajsano=seq(2019,2022,1))
+napoved <- mutate(nova_leta, n=predict(pril_lm, nova_leta))
+
+graf_napoved <- ggplot(tabela_centri, aes(Sezona_okrajsano,n)) + geom_smooth(method="lm", fullrange=TRUE)+
+  geom_point(data=napoved,aes(Sezona_okrajsano,n),color="red", size=2)+
+  geom_point() + ggtitle("Napoved števila centrov kot igralcev tedna") + xlab("Sezona")+ylab("Število")
