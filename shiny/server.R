f29ac721dba6488568d510816d3d7be16818865b
Darjan Pavšič <darjan.pavsic@gmail.com>
2019-03-06 18:08:50 +0100

    Dodan Shiny

--- shiny/server.R
+++ shiny/server.R
@@ -1,29 +1,20 @@
 library(shiny)
 
-shinyServer(function(input, output) {
-  output$druzine <- DT::renderDataTable({
-    dcast(druzine, obcina ~ velikost.druzine, value.var="stevilo.druzin") %>%
-      rename(`Občina`=obcina)
-  })
-  
-  output$pokrajine <- renderUI(
-    selectInput("pokrajina", label="Izberi pokrajino",
-                choices=c("Vse", levels(obcine$pokrajina)))
-  )
-  output$naselja <- renderPlot({
-    main <- "Pogostost števila naselij"
-    if (!is.null(input$pokrajina) && input$pokrajina %in% levels(obcine$pokrajina)) {
-      t <- obcine %>% filter(pokrajina == input$pokrajina)
-      main <- paste(main, "v regiji", input$pokrajina)
-    } else {
-      t <- obcine
-    }
-    ggplot(t, aes(x=naselja)) + geom_histogram() +
-      ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
+
+# 
+# shinyServer(function(input,output){
+#   output$igralci_tedna <- renderTable({
+#     ekipa_filter <- subset(igralci_tedna, igralci_tedna$Igralceva_ekipa == input$id_klub)
+#     
+#   })
+# })
+
+
+
+shinyServer(function(input,output){
+  output$graf1 <- renderPlot({
+    graf_shiny(input$id_poz)
   })
 })
-
-
-
 
 
