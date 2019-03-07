3f1b8d1d83e67b8258bf9fdbed142b5e9c2ab69f
Darjan Pavšič <darjan.pavsic@gmail.com>
2019-03-07 19:05:35 +0100

    Shiny sedaj deluje

--- shiny/server.R
+++ shiny/server.R
@@ -1,19 +1,12 @@
 library(shiny)
 
 
-# 
-# shinyServer(function(input,output){
-#   output$igralci_tedna <- renderTable({
-#     ekipa_filter <- subset(igralci_tedna, igralci_tedna$Igralceva_ekipa == input$id_klub)
-#     
-#   })
-# })
-
-
-
 shinyServer(function(input,output){
   output$graf1 <- renderPlot({
-    graf_shiny(input$id_poz)
+    ggplot(data=igralci_tedna_pozicijefilt %>% 
+             filter(Pozicija == input$id_poz), aes(x=Sezona_okrajsano)) +xlab("Leto") + ylab("Število") + 
+      geom_histogram(binwidth =1, color="gray30", fill="gray30") + theme_bw() + 
+      ggtitle("Osvojeni nazivi igralca tedna glede na pozicijo igranja skozi čas")
   })
 })
 
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,36 +1,13 @@
 library(shiny)
 
 
-graf_shiny <- ggplot(data=igralci_tedna_pozicijefilt,aes(x=igralci_tedna_pozicijefilt$Sezona_okrajsano)) + 
-  geom_histogram(binwidth=1, color="gray30", fill="gray30")
-
-
-
-
-
-
-# fluidPage(
-#   titlePanel("Igralci tedna"),
-#   sidebarLayout(
-#     sidebarPanel(
-#       selectInput("id_klub","Izberi klub", choices=igralci_tedna$Igralceva_ekipa)
-#     ),
-#     
-#     mainPanel(
-#       tableOutput("igralci_tedna")
-#     )
-#   )
-# )
-
-
-  
   
 fluidPage(
   titlePanel("Igralci tedna po pozicijah"),
     sidebarPanel(
-      selectInput("id_poz", "Izberi pozicijo", choices = igralci_tedna_pozicijefilt$Pozicija)
+      selectInput("id_poz", "Izberi pozicijo", choices = unique(igralci_tedna_pozicijefilt$Pozicija))
     ),
-    mainPanel(plotOutput("graf_shiny"))
+    mainPanel(plotOutput("graf1"))
 )
 
       
